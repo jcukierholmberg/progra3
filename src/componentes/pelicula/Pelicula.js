bb9bb12de2e45e6ae778cd77e120b510c67bc1fb
jcukierholmberg <jcukierholmberg@udesa.edu.ar>
2021-09-01 10:36:28 -0300

    algo

--- src/componentes/pelicula/Pelicula.js
+++ src/componentes/pelicula/Pelicula.js
@@ -4,8 +4,10 @@ import './Pelicula.css' ;
 function Pelicula() {
     return (
         <div className= "tarjetaPelicula"> 
-
+            
+            <h2> imagen </h2>
             <h2 className="tituloPelicula"> Titulo de la pelicula </h2>
+            <p> descrpcion de la pelicula</p>
 
         </div>
     
