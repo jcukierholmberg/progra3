8e2144fb351378a0a903a94dbc4a23b5d306418f
kiaranderson <kikuanderson@gmail.com>
2021-09-02 10:13:01 -0300

    probando

---
+++ src/componentes/pelicula/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './Pelicula.css' ;
+
+function Nav() {
+    return (
+        <div className= "Nav"> 
+            
+        </div>
+    
+        );
+}
+
+export default Nav ;
