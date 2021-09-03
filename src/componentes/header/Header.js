7447f04b474340f2f31a79f1fc806e66cb34ef78
jcukierholmberg <jcukierholmberg@udesa.edu.ar>
2021-09-03 12:41:58 -0300

    header

--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -16,11 +16,11 @@ function Header() {
             <button className="Nav-menu"> Columnas </button>
             
             <div class="input-group rounded">
-              <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                aria-describedby="search-addon" />
-              <span class="input-group-text border-0" id="search-addon">
-                <i class="fas fa-search"></i>
-              </span>
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+                  aria-describedby="search-addon" />
+                <span class="input-group-text border-0" id="search-addon">
+                  <i class="fas fa-search"></i>
+                </span>
             </div>
 
           </div>
