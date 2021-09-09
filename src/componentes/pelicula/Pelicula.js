25eeadff1df3d218020ceba01ab040560b86c22e
kiaranderson <kikuanderson@gmail.com>
2021-09-09 10:48:45 -0300

    fetch hecho

--- src/componentes/pelicula/Pelicula.js
+++ src/componentes/pelicula/Pelicula.js
@@ -1,31 +1,44 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import './Pelicula.css' ;
 
-function Pelicula() {
-    let apikey = "api_key=c2f0cd4705d9cf0edf12534fc1a19f65";
-    let url = "https://developers.themoviedb.org/3/movies/get-popular-movies";
 
-    fetch(url + apikey)
-    .then(function(response){
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data);
-        //que hago
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+class Pelicula extends Component{
 
+    constructor(){
+        super()
+        this.state = {
+        }
+    }
+    
+    componentDidMount(){
+
+    let apikey = "?api_key=c2f0cd4705d9cf0edf12534fc1a19f65";
+    let url = "https://api.themoviedb.org/3/movie/popular";
+
+    fetch(url + apikey + "&page=3")
+    .then(response => response.json())
+    .then (data => {
+        console.log(data)
+        this.setState({
+            titulo: data.results.title,
+        })
+    })
+    .catch(error => console.log(error))
+
+}
+
+render(){
     return (
         <div className= "tarjetaPelicula"> 
             
             <h2> imagen </h2>
-            <h2 className="tituloPelicula"> Titulo de la pelicula </h2>
+            <h2 className="tituloPelicula"> Titulo </h2>
             <p> descrpcion de la pelicula</p>
 
         </div>
     
         );
+}
+
 }
 export default Pelicula ;
