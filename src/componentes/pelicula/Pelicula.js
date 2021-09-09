1adb242259e20133f8eb5d278f62d2e0ae85afdc
flor <floraguirre@udesa.edu.ar>
2021-09-09 10:08:53 -0300

    fetch

--- src/componentes/pelicula/Pelicula.js
+++ src/componentes/pelicula/Pelicula.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './Pelicula.css' ;
 
 function Pelicula() {
+    let apikey = "api_key=c2f0cd4705d9cf0edf12534fc1a19f65";
+    let url = "https://developers.themoviedb.org/3/movies/get-popular-movies";
+
+    fetch(url + apikey)
+    .then(function(response){
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data);
+        //que hago
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+
     return (
         <div className= "tarjetaPelicula"> 
             
