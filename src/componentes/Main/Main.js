df00076385ceb41ccad78d061f79da76a840ed34
jcukierholmberg <jcukierholmberg@udesa.edu.ar>
2021-09-14 11:56:57 -0300

    columnas

--- src/componentes/Main/Main.js
+++ src/componentes/Main/Main.js
@@ -12,7 +12,7 @@ class Main extends Component{
             nextURL: '',
             peliculasFiltro: [],
             button: "Ordenar por columnas",
-            filas: false,
+            filas: true,
         }
     }
 
