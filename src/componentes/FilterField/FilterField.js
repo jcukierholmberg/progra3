8210d79d8e4a9a7cc686ccc61e44db465bf403f3
kiaranderson <kikuanderson@gmail.com>
2021-09-16 19:30:20 -0300

    push

--- src/componentes/FilterField/FilterField.js
+++ src/componentes/FilterField/FilterField.js
@@ -25,13 +25,8 @@ class FilterField extends Component {
 
     render(){
         return(
-<<<<<<< HEAD
-            <form action="" onSubmit={()=>this.evitarSubmit()}>
-                <input type="text" onChange={(f)=>this.controlarCambios(f)} value={this.state.filterBy} placehoder="Filtrar pelicula"/>
-=======
-            <form action="" onSubmit={(m)=>this.evitarSubmit(m)}>
+        <form action="" onSubmit={(m)=>this.evitarSubmit(m)}>
                 <input className="input" type="text" onChange={(f)=>this.controlarCambios(f)} value={this.state.filterBy} placeholder="Buscar pelicula"/>
->>>>>>> de95889bbcc169975e1df7f1cc5bd36965013a85
                 <span className="input-group-text border-0" id="search-addon">
                     <i className="fas fa-search buscador" ></i>
                 </span>
