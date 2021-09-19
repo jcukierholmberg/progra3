b830459548aa972c47ec65f3a66ca83e7d9629a1
kiaranderson <kikuanderson@gmail.com>
2021-09-19 16:32:50 -0300

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import React from "react";
 import Main from './componentes/Main/Main';
 import Footer from './componentes/Footer/Footer';
-import Head from './componentes/Head/Head';
 
 function App(){
   return(
     <React.Fragment>
-      <Head />
       <div className="App">
           <Main />
       <footer>
