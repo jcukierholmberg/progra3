15d76626efab5cc5340742873c397ae050e55518
jcukierholmberg <jcukierholmberg@udesa.edu.ar>
2021-10-12 11:40:11 -0300

    jkl

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css' ;
 function Footer() {
     return(
     <h2 className="text">
-        Joaquin Cukierr,
+        Joaquin Cukier,
         Kiara Anderson,
         Flor Aguirre
     </h2>
